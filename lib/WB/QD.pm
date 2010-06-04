15410f04a42f2314ac2c8c2c6cea83d1d179b857
Andreas Barth <aba@not.so.argh.org>
2010-06-04 10:04:54 +0000

    add merge-v3: + call with installed-packages+ . installed-sources+ [ . available-for-build-packages+ ] + can cope with any filetype (including compressed) + allows to specify installed packages which are not available for building (and the other way round) + does quinn-diff internally, for that:   + add WB/QD.pm plus testcase in tests/qd.pl + reads packages / sources only once + uses edos to resolve dep-waits + respect $simulate
    enhance call_edos_depcheck:
    + add possibility to check depwaits (flag depwait)
    + respect $simulate

---
+++ lib/WB/QD.pm
@@ -0,0 +1,130 @@
+package WB::QD;
+
+use strict;
+use IO::Uncompress::AnyInflate qw(anyinflate);
+use Dpkg::Version qw(vercmp);
+
+sub readsourcebins {
+    my $arch = shift;
+    my $pasfile = shift;
+    my $SRC = shift;
+    my $BIN = shift;
+    my $binary = {};
+
+    my $pas = {};
+    local($/) = "\n";
+    open(PAS, '<', $pasfile);
+    while(<PAS>) {
+        chomp;
+        s,\s*#.*,,;
+        next unless $_;
+        my ($p, $c) = split(/:\s*/);
+        $pas->{$p} = { arch => [ split(/\s+/, $c) ], mode => substr($c, 0, 1) ne '!' };
+    }
+    close PAS;
+
+    my $srcs = {};
+    local($/) = ""; # read in paragraph mode
+
+    foreach my $s (@$SRC) {
+        my $S = new IO::Uncompress::AnyInflate($s) || return "can't open $s";
+        while(<$S>) {
+            my $p={};
+            /^Package:\s*(\S+)$/mi and $p->{'name'} = $1;
+            /^Version:\s*(\S+)$/mi and $p->{'version'} = $1;
+            /^Binary:\s*(.*)$/mi and $p->{'binary'} = $1;
+            /^Architecture:\s*(.+)$/mi and $p->{'arch'} = $1;
+            /^Priority:\s*(\S+)$/mi and $p->{'priority'} = $1;
+            /^Section:\s*(\S+)$/mi and $p->{'section'} = $1;
+            /^Build-Depends:\s*(.*)$/mi and $p->{'depends'} = $1;
+            /^Build-Conflicts:\s*(.*)$/mi and $p->{'conflicts'} = $1;
+
+            next unless $p->{'name'} and $p->{'version'};
+            next if $p->{'arch'} eq 'all';
+            # TODO: respect the architecture - or not / we already respect it via P-a-s
+            delete $p->{'arch'};
+
+            # ignore if package already exists with higher version
+            if ($srcs->{$p->{'name'}}) {
+                next if (vercmp($srcs->{$p->{'name'}}->{'version'}, $p->{'version'}) > 0);
+            }
+            my @a = split(/,? /, $p->{'binary'}) if $p->{'binary'};
+            $p->{'binary'} = \@a;
+            $srcs->{$p->{'name'}} = $p;
+        }
+        close $S;
+    }
+
+    foreach my $p (@$BIN) {
+        my $P = new IO::Uncompress::AnyInflate($p) || return "can't open $p";
+        while(<$P>) {
+            my $p;
+            /^Version:\s*(\S+)$/mi and $p->{'version'} = $1;
+            /^Architecture:\s*(\S+)$/mi and $p->{'arch'} = $1;
+            /^Package:\s*(\S+)$/mi and $p->{'binary'} = $1;
+            /^Package:\s*(\S+)$/mi and $p->{'source'} = $1;
+            /^Source:\s*(\S+)$/mi and $p->{'source'} = $1;
+            /^Source:\s*(\S+)\s+\((\S+)\)$/mi and $p->{'source'} = $1 and $p->{'version'} = $2;
+            $p->{'version'} =~ /(\S+)\+b([0-9]+)/ and $p->{'version'} = $1 and $p->{'binnmu'} = $2;
+
+            $binary->{$p->{'binary'}} = { 'version' => $p->{'version'}, 'arch' => $p->{'arch'}} unless $binary->{$p->{'binary'}} and vercmp($binary->{$p->{'binary'}->{'version'}}, $p->{'version'}) < 0;
+
+            #next if $pas->{$p->{'binary'}} && pasignore($pas->{$p->{'binary'}}, $arch);
+            next if $p->{'arch'} eq 'all';
+            next unless $srcs->{$p->{'source'}};
+            $srcs->{$p->{'source'}}->{'compiled'} = 1;
+            next unless $srcs->{$p->{'source'}}->{'version'} eq $p->{'version'};
+            $srcs->{$p->{'source'}}->{'installed'} = 1;
+            next unless $p->{'binnmu'};
+            next if ($srcs->{$p->{'source'}}->{'binnmu'}) && ($srcs->{$p->{'source'}}->{'binnmu'} > $p->{'binnmu'});
+            $srcs->{$p->{'source'}}->{'binnmu'} = $p->{'binnmu'};
+        }
+        close $P;
+    }
+
+    SRCS:
+    for my $k (keys %$srcs) {
+        if ($srcs->{$k}->{'installed'}) {
+            $srcs->{$k}->{'status'} = 'installed';
+            delete $srcs->{$k}->{'installed'};
+        } elsif ($srcs->{$k}->{'compiled'}) {
+            $srcs->{$k}->{'status'} = 'out-of-date';
+        } else {
+            $srcs->{$k}->{'status'} = 'uncompiled';
+        }
+        delete $srcs->{$k}->{'compiled'};
+        $srcs->{$k}->{'status'} = 'installed' if $srcs->{$k}->{'arch'} && $srcs->{$k}->{'arch'} eq 'all';
+
+        #my $p = $pas->{$k};
+        #$p ||= $pas->{'%'.$k};
+        #$srcs->{$k}->{'status'} = 'not-for-us' if pasignore($p, $arch);
+        if (pasignore($pas->{'%'.$k}, $arch)) {
+            $srcs->{$k}->{'status'} = 'not-for-us';
+            next;
+        }
+        for my $bin (@{$srcs->{$k}->{'binary'}}) {
+            next if pasignore($pas->{$bin}, $arch);
+            next if $binary->{$bin} and $binary->{$bin}->{'arch'} eq 'all';
+            next SRCS;
+        }
+        $srcs->{$k}->{'status'} = 'not-for-us';
+    }
+    $srcs->{'_binary'} = $binary;
+    local($/) = "\n";
+
+    return \$srcs;
+}
+
+sub pasignore {
+    my $p = shift;
+    my $arch = shift;
+    if ($p && $p->{'mode'}) {
+        return 1 unless grep { $_ eq $arch } @{$p->{'arch'}};
+    }
+    if ($p && not $p->{'mode'}) {
+        return 1 if grep /^!$arch$/, @{$p->{'arch'}};
+    }
+    return 0;
+}
+
+1;
---
+++ tests/qd.pl
@@ -0,0 +1,191 @@
+#!/usr/bin/perl -w -I lib
+
+use Test::More tests => 1;
+
+$src = <<EOF
+Package: bash
+Binary: bash, bash-static, bash-builtins, bash-doc, bashdb
+Version: 4.1-3
+Priority: required
+Architecture: any
+Section: shell
+
+Package: test
+Binary: test
+Version: 4.1-3
+Priority: optional
+Architecture: any
+Section: shell
+
+Package: nfu
+Binary: nfu
+Version: 4.1-3
+Priority: optional
+Architecture: any
+Section: shell
+
+Package: all
+Binary: all
+Version: 4.1-3
+Priority: optional
+Architecture: all
+Section: shell
+
+Package: allbin
+Binary: allbin
+Version: 4.1-3
+Priority: optional
+Architecture: all
+Section: shell
+
+Package: e2fsprogs
+Binary: e2fsck-static
+Version: 1.41.12-1
+Priority: required
+Section: admin
+Architecture: any
+
+Package: e2fsprogs
+Binary: e2fsck-static
+Version: 1.41.11-1
+Priority: required
+Section: admin
+Architecture: any
+
+Package: base-files
+Binary: base-files
+Version: 5.7
+Priority: required
+Section: admin
+Architecture: i386 all
+Build-Depends: debhelper [i386 amd64], bash [!kfreebsd-i386], perl [!i386 !amd64]
+
+Package: pbuilder
+Binary: pbuilder, pbuilder-uml
+Version: 0.196
+Priority: extra
+Section: devel
+Build-Depends: debhelper (>= 7), docbook-xsl, ldp-docbook-xsl (>= 0.0.20040321-0.1), xsltproc, dpkg-dev (>= 1.13.19), dblatex, debconf | debconf-2.0, po-debconf, po4a
+Build-Conflicts: dash
+Architecture: all i386 amd64
+
+EOF
+;
+
+$bin = <<EOF
+Package: base-files
+Priority: required
+Section: admin
+Architecture: i386
+Version: 5.6
+
+Package: bash
+Priority: required
+Section: shells
+Architecture: i386
+Version: 4.1-3+b2
+
+Package: bash-static
+Priority: required
+Section: shells
+Architecture: i386
+Version: 4.1-3+b1
+
+Package: e2fsprogs
+Priority: required
+Section: admin
+Architecture: i386
+Version: 1.41.11-1
+
+Package: all
+Version: 4.1-3
+Priority: optional
+Architecture: all
+Section: shell
+
+Package: pbuilder
+Priority: extra
+Section: devel
+Architecture: all
+Version: 0.196
+
+EOF
+;
+
+$pas = <<EOF
+%nfu: amd64 # we fake to be i386 here
+e2fsprogs: i386
+pbuilder-uml: amd64                                              # depends on user-mode-linux
+EOF
+;
+
+use WB::QD;
+my $srcs = WB::QD::readsourcebins('i386', \$pas, [\$src], [\$bin]);
+use Data::Dumper;
+
+#print Dumper $srcs;
+is_deeply ($$srcs, {
+            'e2fsprogs' => {
+                             'priority' => 'required',
+                             'status' => 'out-of-date',
+                             'version' => '1.41.12-1',
+                             'name' => 'e2fsprogs',
+                             'section' => 'admin',
+                             'binary' => ['e2fsck-static', ],
+                           },
+            'bash' => {
+                        'priority' => 'required',
+                        'status' => 'installed',
+                        'version' => '4.1-3',
+                        'name' => 'bash',
+                        'section' => 'shell',
+                        'binnmu' => '2',
+                             'binary' => ['bash', 'bash-static', 'bash-builtins', 'bash-doc', 'bashdb'],
+                      },
+            'base-files' => {
+                              'priority' => 'required',
+                              'status' => 'out-of-date',
+                              'version' => '5.7',
+                              'name' => 'base-files',
+                              'section' => 'admin',
+                              'depends' => 'debhelper [i386 amd64], bash [!kfreebsd-i386], perl [!i386 !amd64]',
+                             'binary' => ['base-files'],
+                            },
+            'test' => {
+                        'priority' => 'optional',
+                        'status' => 'uncompiled',
+                        'version' => '4.1-3',
+                        'name' => 'test',
+                        'section' => 'shell',
+                             'binary' => ['test'],
+                      },
+            'nfu' => {
+                        'priority' => 'optional',
+                        'status' => 'not-for-us',
+                        'version' => '4.1-3',
+                        'name' => 'nfu',
+                        'section' => 'shell',
+                             'binary' => ['nfu'],
+                      },
+            'pbuilder' => {
+                            'priority' => 'extra',
+                            'status' => 'not-for-us',
+                            'version' => '0.196',
+                            'binary' => [
+                                          'pbuilder',
+                                          'pbuilder-uml'
+                                        ],
+                            'name' => 'pbuilder',
+                            'section' => 'devel',
+                            'depends' => 'debhelper (>= 7), docbook-xsl, ldp-docbook-xsl (>= 0.0.20040321-0.1), xsltproc, dpkg-dev (>= 1.13.19), dblatex, debconf | debconf-2.0, po-debconf, po4a',
+                            'conflicts' => 'dash'
+                          },
+            '_binary' => {
+                'e2fsprogs' => {'version' => '1.41.11-1', 'arch' => 'i386'},
+                'bash' => {'version' => '4.1-3', 'arch' => 'i386'},
+                'base-files' => {'version' => '5.6', 'arch' => 'i386'},
+                'bash-static' => {'version' => '4.1-3', 'arch' => 'i386'},
+                'all' => {'version' => '4.1-3', 'arch' => 'all'},
+                'pbuilder' => { 'arch' => 'all', 'version' => '0.196' },
+            },
+}, 'reading packages');
