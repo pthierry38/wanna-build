ec65a5318cc0d6791ee168b8ca1627b0f0398e14
Debian wanna-build admins <debian-wb-team@lists.debian.org>
2011-02-07 18:38:37 +0000

    update-schema.sh: add git push post-commit

--- schema/update-schema.sh
+++ schema/update-schema.sh
@@ -8,5 +8,6 @@ git add wanna-build.sql
 sudo -u postgres pg_dumpall --cluster 8.4/wanna-build | egrep '^(GRANT.*TO.*BY|CREATE ROLE)' > roles.sql
 git add roles.sql
 
-git commit wanna-build.sql roles.sql -m 'Auto-committed schema changes.'
+git status >/dev/null && git commit wanna-build.sql roles.sql -m 'Auto-committed schema changes.'
+git push >/dev/null
 
