b37ff3c86c59ecd92e3a7e019c710a23baef5996
Aurelien Jarno <aurelien@aurel32.net>
2014-08-18 09:46:36 +0200

    Add ppc64el architecture

--- schema/arches-tables.sh
+++ schema/arches-tables.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ARCHES="alpha amd64 arm armel arm64 hppa i386 ia64 kfreebsd-amd64 kfreebsd-i386 mips mipsel powerpc s390 sparc"
+ARCHES="alpha amd64 arm armel arm64 hppa i386 ia64 kfreebsd-amd64 kfreebsd-i386 mips mipsel powerpc ppc64el s390 sparc"
 
 rm -f arches-tables.sql
 for arch in $ARCHES; do sed -e "s/ARCH/$arch/g" < arches-tables.in >> arches-tables.sql ; done
